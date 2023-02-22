fbda1a8142762bc77c982b2f9e8fa7886de2a91e
Dilafruz7777777 <dilafruzasamutdinovaflutter@gmail.com>
2023-02-22 18:22:26 +0500

    set up counter_cubit.dart, counter_page.dart

---
+++ lib/bloc_cubit/CounterCubit.java
@@ -0,0 +1,13 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class NameCubit extends Cubit<String> {
+  CounterCubit() : super(0);
+
+  void increment() {
+    emit(state name);
+  }
+
+//  void decrement(){
+//    emit(state -1);
+//  }
+}
